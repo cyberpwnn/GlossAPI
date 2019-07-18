a65deabce3104ef8ce00e165815d254bd9a7bd78
Dave <DavidRockin@users.noreply.github.com>
2019-07-17 23:18:21 -0400

    filter will require player instance

--- src/main/java/com/volmit/gloss/api/display/FilteredRenderer.java
+++ src/main/java/com/volmit/gloss/api/display/FilteredRenderer.java
@@ -1,6 +1,10 @@
 package com.volmit.gloss.api.display;
 
+import org.bukkit.entity.Player;
+
 public interface FilteredRenderer extends FilterChain
 {
 	public String filter(String r);
+
+	public String filter(String r, Player p);
 }
