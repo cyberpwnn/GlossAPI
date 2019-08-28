03ece9c6fb04a69796906dbf95ace38737331749
Dave <DavidRockin@users.noreply.github.com>
2019-08-28 12:31:13 -0400

    emoji isEnabled and canUse methods

--- src/main/java/com/volmit/gloss/api/display/Emoji.java
+++ src/main/java/com/volmit/gloss/api/display/Emoji.java
@@ -2,10 +2,15 @@ package com.volmit.gloss.api.display;
 
 import com.volmit.gloss.api.util.IDD;
 import com.volmit.gloss.api.util.IJSON;
+import org.bukkit.entity.Player;
 
 public interface Emoji extends IDD, IJSON
 {
+	public boolean isEnabled();
+
 	public String getTrigger();
 
 	public String getEmoji();
+
+	public boolean canUse(Player player);
 }
